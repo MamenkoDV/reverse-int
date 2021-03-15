2de03a01b0d27ecbea3bdf2e090efbce105d76dd
Dmytro Mamenko <mamenko.dv@gmail.com>
2021-03-15 23:42:09 +0100

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    const result = n.toString().split("");
+    return +result.reverse().join("");
+};
